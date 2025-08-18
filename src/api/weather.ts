311ffbbeead3c25a23596dabff2aad2a2d3b8978
Shaikh-Suja-Rahaman <sujarahaman.dms.727@gmail.com>
2025-08-18 17:51:50 +0530

    API Handling on a scallable way

---
+++ src/api/weather.ts
@@ -0,0 +1,69 @@
+import { API_CONFIG } from "./config";
+import type { Coordinates, ForecastData, GeocodingResponse, WeatherData } from "./type";
+
+//this code over here makes this very very scalable and neat
+//who knew even api calling can be this complex yet proper
+class WeatherAPI {
+
+
+  //classes in ts
+  private createURL(endpoint: string, params: Record<string, string | number>) {
+    //so endpoint is a variable of type string, and params is an object (aka record) which can either have string, string key value pair or string, number
+    const searchParams = new URLSearchParams({
+      //URLSearchParms is like magic, takes an object and makes it url friendly
+      appid: API_CONFIG.API_KEY, //my api key
+      ...params, //object
+    });
+    return `${endpoint}?${searchParams.toString()}`; //toString does the magic
+    //the order doesnt matter, appid could be at the beginning or at the end
+  } //this just takes my endpoint url and then adds the parameters like city and units in a fitting way. so that it fits the url
+
+
+
+
+  private async fetchData<T>(url: string): Promise<T> {
+    //<T> is generic TYPE, so i'll declare this during calling this function, its like a placeholder
+    // now colon: promise<T> means that this function's return type is a promise and once that promise is resolved, it would return the <T> type which I had entered before, this is just for error rectification and to help typeScript catch errors
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Weather API Error: ${response.statusText}`);
+    }
+
+    return response.json();
+  }
+
+  async getCurrentWeather({ lat, lon }: Coordinates): Promise<WeatherData> {
+    const url = this.createURL(`${API_CONFIG.BASE_URL}/weather`, { //calling the create url function
+      lat: lat.toString(),
+      lon: lon.toString(),
+      units: API_CONFIG.DEFAULT_PARAMS.units,
+    });
+
+    return this.fetchData<WeatherData>(url); //fetching the data using the fetchData function
+  }
+
+  async getForecast({ lat, lon }: Coordinates): Promise<ForecastData> {
+    const url = this.createURL(`${API_CONFIG.BASE_URL}/forecast`, { //calling the create url function
+      lat: lat.toString(),
+      lon: lon.toString(),
+      units: API_CONFIG.DEFAULT_PARAMS.units,
+    });
+
+    return this.fetchData<ForecastData>(url); //fetching the data using the fetchData function
+  }
+
+  async reverseGeocode({lat, lon}: Coordinates):Promise<GeocodingResponse[]> {
+    const url = this.createURL(
+      `API_CONFIG.GEO/reverse`, {
+        lat: lat.toString(),
+        lon: lon.toString(),
+        limit :  1, //i just need the first object as it returns an array //string -> number
+      }
+    );
+
+    return this.fetchData<GeocodingResponse[]>(url);
+  }
+}
+
+export const weatherAPI = new WeatherAPI(); //this obejct is all we need to access our api end points
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import { Moon, Sun } from "lucide-react";
 import { useTheme } from "../context/theme-provider";
 import type { FC } from "react";
 import { Link } from "react-router-dom";
@@ -12,15 +13,18 @@ const Header: FC = () => {
           <img
             src="/cloudy-day Dark.png"
             alt="Cloudy day logo"
-            className="h-14"
+            className="h-10 "
             style={
               isDark ? { filter: "brightness(0) invert(1)" } : {}
             }
           />
 
         </Link>
-          <div onClick={()=>setTheme(isDark?"light":"dark")}>
-            toggle
+          <div onClick={()=>setTheme(isDark?"light":"dark")}
+            className={`flex items-center cursor-pointer transition-transform duration-500 ${isDark ? "rotate-180": " rotate-0"}`}
+
+            >
+            {isDark? <Sun className="h-6 text-yellow-500 "/> : <Moon className="h-6 text-blue-500"/>}
           </div>
       </div>
     </header>
